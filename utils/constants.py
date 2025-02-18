d7a3a4124c9d8777d32f08e4d158ec98f9f388f9
Protik Banerji <protik09@users.noreply.github.com>
2025-02-18 05:21:48 +0100

    Change model depending on availability of GPU VRAM

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,7 @@
 # Description: Constants used in the project
 import os
 from typing import List
+import torch
 
 DEFAULT_INPUT_VIDEO: str = os.path.join("assets", "input.mp4")
 
@@ -20,4 +21,32 @@ MODELS_AVAILABLE: List[str] = [
 ]
 
 # Use this model for transcription
-MODEL_SIZE: str = "base.en"
+# Check to see how much VRAM is available on your GPU and select the model accordingly
+# tiny.en: 1.5 GB
+# base.en: 2.5 GB
+# small.en: 3.5 GB
+# medium.en: 5 GB
+# large-v1: 8.0 GB
+# large-v2: 8.0 GB
+# large-v3: 8.0 GB
+
+
+# Check GPU VRAM and select appropriate model
+if torch.cuda.is_available():
+    vram_gb = round((torch.cuda.get_device_properties(0).total_memory / 1.073742e+9), 1)
+    print(f"Detected VRAM: {vram_gb} GB")
+    if vram_gb >= 8.0:
+        MODEL_SIZE = "large-v3"
+    elif vram_gb >= 5.0:
+        MODEL_SIZE = "medium.en"
+    elif vram_gb >= 3.5:
+        MODEL_SIZE = "small.en"
+    elif vram_gb >= 2.5:
+        MODEL_SIZE = "base.en"
+    else:
+        MODEL_SIZE = "tiny.en"
+else:
+    MODEL_SIZE = "tiny.en"  # Fallback if no GPU is available
+
+print(f"Using model size: {MODEL_SIZE}")
+# MODEL_SIZE: str = "large-v3"  # Use this model for transcription
