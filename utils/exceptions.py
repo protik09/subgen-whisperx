4157c2597a88d3d078b996133277de5d61ed42c0
Protik Banerji <protik09@users.noreply.github.com>
2025-03-08 03:20:04 +0100

    Add more custom Exceptions

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -26,3 +26,27 @@ class MediaNotFoundError(Exception):
 
     def __str__(self):
         return f"{self.message}"
+
+class InvalidThreadCountError(Exception):
+    """
+    Exception raised when an invalid thread count is specified.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+class InvalidPathError(Exception):
+    """
+    Exception raised when a path is invalid or potentially malicious.
+    """
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
